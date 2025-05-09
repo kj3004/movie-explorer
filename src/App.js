2eaaa6936b785519ea546c148ab510b5af54d1eb
kj3004 <137755543+kj3004@users.noreply.github.com>
2025-05-09 11:28:31 +0530

    adjusted routings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import { MovieProvider } from "./context/MovieContext";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MovieDetails from "./components/MovieDetails";
+import FavoritesPage from "./pages/FavoritesPage";
+import { CssBaseline } from "@mui/material";
 
-function App() {
+const PrivateRoute = ({ children }) => {
+  const isAuthenticated = localStorage.getItem("isAuthenticated");
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <MovieProvider>
+        <Router>
+          <CssBaseline />
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/movie/:id"
+              element={
+                <PrivateRoute>
+                  <MovieDetails />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/favorites"
+              element={
+                <PrivateRoute>
+                  <FavoritesPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </MovieProvider>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
